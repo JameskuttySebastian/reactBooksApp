c17efb9f086b27fc0b9adf34ce77b1b44c16db9a
Jameskutty Sebastian <jameskutty.sebastian@gmail.com>
2020-03-28 22:38:59 +1030

    uploading to heroku

---
+++ client/build/precache-manifest.effeabc6ac172ad9834a499bdb20c4d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "790eab1bc39ff23e9b34",
+    "url": "/static/css/main.18a8f443.chunk.css"
+  },
+  {
+    "revision": "790eab1bc39ff23e9b34",
+    "url": "/static/js/main.f3c826e2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cb17bcf018bf7c55997e",
+    "url": "/static/js/2.09d13a7d.chunk.js"
+  },
+  {
+    "revision": "ffce6cb51f6b6474c58c0457ccafcd23",
+    "url": "/index.html"
+  }
+];
